e9ff905bca4a9d1dd8adfd9d3f41249b1c590bf4
SeanWu2013 <sww35@scarletmail.rutgers.edu>
2016-03-25 01:14:54 -0400

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	account.php

--- account.php
+++ account.php
@@ -88,48 +88,48 @@
 				<p>
 					<h2>My Info</h2>
                         <?php
+                        //start session
                         session_start();
+
                         //connect to SQL host
-                        $host = "localhost";
-                        $SQLusername = "root";
-                        $password = "";
-                        $database = "parkingGarage";
+						define('_HOST_NAME_', 'localhost');
+						define('_USER_NAME_', 'root');
+						define('_DB_PASSWORD', '');
+						define('_DATABASE_NAME_', 'parkinggarage');
                         try {
-                            $connection = new PDO("mysql:host=$host;database=$database", $SQLusername, $password);
+                            $connection = new PDO('mysql:host='._HOST_NAME_.';dbname='._DATABASE_NAME_, _USER_NAME_, _DB_PASSWORD);
                             $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                         }
                         catch (PDOException $err)
                         {
                             echo $err->getMessage();
                         }
-
                         //store the username in a variable for use in SQL commands
                         $user = htmlspecialchars($_SESSION['username']);
                         //print username and password
-                        echo "Username: $user<br>";
+                        echo "Username: $user <br>";
                         echo "Password: ****** <br>";
-
                         //send SQL queries to the database and print the result
                         //print the balance
-                        $balanceSQL = $connection->prepare("SELECT Balance FROM accounts WHERE Username = $user");
+                        $balanceSQL = $connection->prepare("SELECT Balance FROM accounts WHERE username = :user");
+                        $balanceSQL->bindParam(':user',$user);
                         $balanceSQL->execute();
                         $balance = $balanceSQL->setFetchMode(PDO::FETCH_ASSOC);
-                        echo "Balance: $balance->fetch()";
-
+                        echo "Balance: $balance <br>"; 
                         //print any reservations
-                        $reservationSQL = $connection->prepare("SELECT Reservation FROM accounts WHERE Username = $user");
+                        $reservationSQL = $connection->prepare("SELECT Reservation FROM accounts WHERE username = :user");
+                        $reservationSQL->bindParam(':user',$user);
                         $reservationSQL->execute();
                         $reservation = $reservationSQL->setFetchMode(PDO::FETCH_ASSOC);
-                        echo "Reservations: $reservation->fetch()";
-
+                        echo "Reservations: $reservation<br>"; 
                         //print license plate of car
-                        $licenseSQL = $connection->prepare("SELECT LicensePlate FROM accounts WHERE Username = $user");
+                        $licenseSQL = $connection->prepare("SELECT LicensePlate FROM accounts WHERE username = :user");
+                        $licenseSQL->bindParam(':user',$user);
                         $licenseSQL->execute();
                         $license = $licenseSQL->setFetchMode(PDO::FETCH_ASSOC);
-                        echo "License Plate: $license->fetch()";
-
+                        echo "License Plate: $license<br>"; 
                         //close connection
-                        //$connection->close();
+                        $connection->close();
                     ?>
                     <br>
 					<a href="editinfo.html">Edit Info</a><p>
@@ -138,14 +138,10 @@
 			</div>
 		</section>
 	</div>
-
-
 			<script src="assets/js/jquery.min.js"></script>
 			<script src="assets/js/skel.min.js"></script>
 			<script src="assets/js/util.js"></script>
 			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
 			<script src="assets/js/main.js"></script>
-
-
 </body>
 </html>
