89e34032a6544faa3d766d59b03e14f23fd33a1c
ab1150 <ab1150@scarletmail.rutgers.edu>
2016-05-08 17:35:19 -0400

    Update reservation.php
    header

--- reservation.php
+++ reservation.php
@@ -2,6 +2,10 @@
 	include("reservationScript.php");
 ?>
 <!DOCTYPE html>
+<!-- //written by: Sean Wu, Sam Yang, Calvin Li and Anushiya Balakrishnan
+//tested by: Calvin Li and Anantha Mahavrathayajula
+//debugged by: Sam Yang and Justen Yeung -->
+
 <html>
 <head>
 <title>Parking Reservation</title>
