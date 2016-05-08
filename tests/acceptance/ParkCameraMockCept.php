048f2fb69b53d13807b50def2f2539fc3a952e56
Calvin <cli.calvinli@gmail.com>
2016-05-08 00:22:04 -0400

    Added parkCameraMock test

---
+++ tests/acceptance/ParkCameraMockCept.php
@@ -0,0 +1,28 @@
+<?php
+
+$I = new AcceptanceTester($scenario);
+//navigate to the page
+$I->amOnPage('/parkCameraMock.php');
+//Check that all the necessary text on the page is present
+$I->seeLink('Automated Garage');
+$I->seeLink('Register');
+$I->seeLink('Log in');
+$I->seeLink('Reservations');
+$I->see('George Street Garage');
+$I->see('Mock Input for a Parking Spot Sensor');
+$I->see('Mock Output for a Parking Spot Sensor');
+$I->see('Plate number:');
+$I->see('Spot number:');
+$I->see('Start Time:');
+//Test the fields
+$I->fillField('spotNum','105');
+$I->fillField('startTime','03-03-2016 05:05:05');
+$I->click('Park');
+
+//test the pull out menu
+$I->click(['class' => 'menu']);
+$I->seeLink('Register');
+$I->seeLink('Make a reservation');
+$I->seeLink('Log in');
+
+$I->wantTo('perform actions and see result');
